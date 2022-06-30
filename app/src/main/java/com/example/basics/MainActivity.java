c69cf27493d937803b9d399aeed5c927a4e03a63
Yeahya35 <yahyaeren35@hotmail.com>
2022-06-30 12:01:02 +0300

    klmdas<ldm

--- app/src/main/java/com/example/basics/MainActivity.java
+++ app/src/main/java/com/example/basics/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
+        System.out.println("check here");
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
     }
